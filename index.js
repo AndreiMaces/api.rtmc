ca2767eaf9d02111699d0c802b64bead6a81453a
AndreiMaces <macesandrei2@gmail.com>
2023-05-01 10:40:17 +0300

    Add html routes

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const fs = require('fs');
+const fs = require("fs");
 
 app.use(cors());
+app.set("view engine", "ejs");
 
 const organizersRaw = fs.readFileSync("organizing_comitee.json");
 const organizers = JSON.parse(organizersRaw);
-app.get("/organizers" , (req, res) => {
-    res.json(organizers);
+app.get("/organizers", (req, res) => {
+  res.json(organizers);
 });
 
 const scientificComiteeRaw = fs.readFileSync("scientific_comitee.json");
 const scientificComitee = JSON.parse(scientificComiteeRaw);
 app.get("/comitee", (req, res) => {
-    res.json(scientificComitee);
+  res.json(scientificComitee);
 });
 
 const participantsRaw = fs.readFileSync("participants.json");
 const participants = JSON.parse(participantsRaw);
 app.get("/participants", (req, res) => {
-    res.json(participants);
-})
+  res.json(participants);
+});
 
 const speakersRaw = fs.readFileSync("speakers.json");
 const speakers = JSON.parse(speakersRaw);
 app.get("/speakers", (req, res) => {
-    res.json(speakers);
-})
+  res.json(speakers);
+});
+
+app.get("/general-info", (req, res) => {
+  res.render("general-info");
+});
+
+app.get("/accomodation", (req, res) => {
+  res.render("accomodation");
+});
 
 app.listen(3010, () => {
-    console.log("Listening on port 3010...");
-})
+  console.log("Listening on port 3010...");
+});
