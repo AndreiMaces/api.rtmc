2f725ae6b6be02a618454617a885e2dab63aaee0
AndreiMaces <macesandrei2@gmail.com>
2023-05-01 07:28:53 +0300

    Add json files && Add basic functions

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fs = require('fs');
+
+app.use(cors());
+
+const organizersRaw = fs.readFileSync("organizing_comitee.json");
+const organizers = JSON.parse(organizersRaw);
+app.get("/organizers" , (req, res) => {
+    res.json(organizers);
+});
+
+const scientificComiteeRaw = fs.readFileSync("scientific_comitee.json");
+const scientificComitee = JSON.parse(scientificComiteeRaw);
+app.get("/comitee", (req, res) => {
+    res.json(scientificComitee);
+});
+
+const participantsRaw = fs.readFileSync("participants.json");
+const participants = JSON.parse(participantsRaw);
+app.get("/participants", (req, res) => {
+    req.json(participants);
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000...");
+})
